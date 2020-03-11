63c5f3ff1d6c3f7d6abd32b88da4135f80005f53
William Mckinnon <willpower3309@gmail.com>
2020-03-11 11:06:11 -0400

    updated motion_report

--- network_creation/network_lib/motion_report.py
+++ network_creation/network_lib/motion_report.py
@@ -32,69 +32,58 @@ import paho.mqtt.client as mqtt
 ####################################
 
 
-def create_motionmatrix_report(network, time_stamp, interval, count, report_type=None):
-    # Create a dummy motion matrix report
-
-    if report_type == "matrix":
-        data_key = "data"
-    else:
-        data_key = "motion"
-
-    report = {
-        "ts": time_stamp,
-        "interval": interval * 100,
-        "count": count,
-        data_key: {"mkai": [], "throughput": []},
-        "links": [],
-    }
-    # Generate link list combinations (using the mesh macs in the network).
-    for i in range(len(network["nodes"]) - 1):
-        for j in range(i + 1, len(network["nodes"])):
-            src_mac = network["nodes"][i]["mesh_mac"]
-            dest_mac = network["nodes"][j]["mesh_mac"]
-            report["links"].append(
-                src_mac.replace(":", "")[-6:]
-                + "-"
-                + dest_mac.replace(":", "")[-6:]
-            )
-
-    for _ in range(len(report["links"])):
-        mkai = []
-        throughput = []
-        for x in range(report["count"]):
-            mkai.extend([random.random()])
-            throughput.extend([random.random()])
-        report[data_key]["mkai"].append(mkai)
-        report[data_key]["throughput"].append(throughput)
-    return report
-
-
-def send_motion_report(network_model, domain):
+def send_motion_report(guardian_type, guardian_id, mqtt_creds, report_data, domain):
     # Report Matrix MQTT topic
     topic = "iot-2/type/%s/id/%s/evt/%s/fmt/json" % (
-        network_model.guardian_type,
-        network_model.guardian_id,
+        guardian_type,
+        guardian_id,
         "motion-matrix",
     )
 
     # MQTT URL
     mqtt_url = "mqtt.%s" % domain
 
-    # Establish persistent connection
-    client = mqtt.Client(client_id=network_model.guardian_id)
-    client.username_pw_set(network_model.mqtt_creds[0], network_model.mqtt_creds[1])
-    client.connect(mqtt_url, 1883, 60)
-    client.loop_start()
-    while True:
-        msg_info = client.publish(topic, json.dumps(
-            create_motionmatrix_report(
-                network_model.network_schema, time.time(), 5, 1, "matrix"
-            )
-        ))
-        if msg_info.rc != 0:
-            print("ERROR: Failed to send mock live data")
-        else:
-            print("send report")
+    # On disconnect, break MQTT loop
+    def on_disconnect(given_client):
+        given_client.loop_stop()
 
-    client.loop_stop()
-    client.disconnect()
+    # Establish connection
+    client = mqtt.Client(client_id=guardian_id)
+    client.username_pw_set(mqtt_creds[0], mqtt_creds[1])
+    client.connect(mqtt_url, 1883, 60)
+    client.on_disconnect = on_disconnect
+    client.loop()
+
+    # Send data
+    msg_info = client.publish(topic, json.dumps(report_data))
+
+    # If message isn't published, return False, and fail test.
+    if not msg_info.is_published():
+        return False
+    # If message is published, return True.
+    if msg_info.is_published():
+        return True
+
+
+def send_report(network_model, domain):
+    with open("network_lib/live_data.json") as json_file:
+        motion_report_data = json.load(json_file)
+    time.sleep(2)
+
+    while 1:
+        motion_report_data["ts"] = time.time()
+
+        result = send_motion_report(
+            network_model.guardian_type,
+            network_model.guardian_id,
+            network_model.mqtt_creds,
+            motion_report_data,
+            domain
+        )
+
+        if not result:
+            print("ERROR: Report Not Sent")
+            return
+        else:
+            print("Report sent")
+        time.sleep(1)
