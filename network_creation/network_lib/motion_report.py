47e2dcf2df55134767bb882e92e395fd0d2abd96
William Mckinnon <willpower3309@gmail.com>
2020-03-11 11:08:50 -0400

    made motion_report code cleaner

--- network_creation/network_lib/motion_report.py
+++ network_creation/network_lib/motion_report.py
@@ -32,11 +32,11 @@ import paho.mqtt.client as mqtt
 ####################################
 
 
-def send_motion_report(guardian_type, guardian_id, mqtt_creds, report_data, domain):
+def send_motion_report(network_model, report_data, domain):
     # Report Matrix MQTT topic
     topic = "iot-2/type/%s/id/%s/evt/%s/fmt/json" % (
-        guardian_type,
-        guardian_id,
+        network_model.guardian_type,
+        network_model.guardian_id,
         "motion-matrix",
     )
 
@@ -48,8 +48,8 @@ def send_motion_report(guardian_type, guardian_id, mqtt_creds, report_data, doma
         given_client.loop_stop()
 
     # Establish connection
-    client = mqtt.Client(client_id=guardian_id)
-    client.username_pw_set(mqtt_creds[0], mqtt_creds[1])
+    client = mqtt.Client(client_id=network_model.guardian_id)
+    client.username_pw_set(network_model.mqtt_creds[0], network_model.mqtt_creds[1])
     client.connect(mqtt_url, 1883, 60)
     client.on_disconnect = on_disconnect
     client.loop()
@@ -66,17 +66,16 @@ def send_motion_report(guardian_type, guardian_id, mqtt_creds, report_data, doma
 
 
 def send_report(network_model, domain):
+    motion_report_data = None
+
     with open("network_lib/live_data.json") as json_file:
         motion_report_data = json.load(json_file)
     time.sleep(2)
 
     while 1:
         motion_report_data["ts"] = time.time()
-
         result = send_motion_report(
-            network_model.guardian_type,
-            network_model.guardian_id,
-            network_model.mqtt_creds,
+            network_model,
             motion_report_data,
             domain
         )
@@ -84,6 +83,5 @@ def send_report(network_model, domain):
         if not result:
             print("ERROR: Report Not Sent")
             return
-        else:
-            print("Report sent")
+
         time.sleep(1)
